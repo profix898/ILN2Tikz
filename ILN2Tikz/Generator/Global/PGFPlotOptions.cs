c3e12d30067636da6afc5b5232783203126f69fe
Thilo Viereck <codedev@profix898.de>
2018-04-03 11:53:19 +0200

    - feature: Custom major/minor grid styles

--- ILN2Tikz/Generator/Global/PGFPlotOptions.cs
+++ ILN2Tikz/Generator/Global/PGFPlotOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using ILNumerics.Drawing;
 
 namespace ILN2Tikz.Generator.Global
@@ -8,9 +9,28 @@ namespace ILN2Tikz.Generator.Global
     {
         public PGFPlotOptions()
         {
+            Add("compat=1.13"); // Minimum version
             Add("set layers"); // Sort layers
             Add("major grid style={solid,very thin,white!80!black}"); // Default major grid style
             Add("minor grid style={dashed,very thin,white!90!black}"); // Default minor grid style
+        }
+
+        public void SetCompatiblity(string version)
+        {
+            // Replace compat version
+            this[0] = "compat=" + version;
+        }
+
+        public void SetMajorGridStyle(Color gridColor, DashStyle gridStyle, int gridWidth)
+        {
+            // Replace default major grid style
+            this[2] = $"major grid style={{{TikzFormatUtility.FormatLine(gridColor, gridStyle, 0.5f * gridWidth)}}}";
+        }
+
+        public void SetMinorGridStyle(Color gridColor, DashStyle gridStyle, int gridWidth)
+        {
+            // Replace default minor grid style
+            this[3] = $"minor grid style={{{TikzFormatUtility.FormatLine(gridColor, gridStyle, 0.5f * gridWidth)}}}";
         }
 
         #region Implementation of ITikzElement
@@ -26,7 +46,12 @@ namespace ILN2Tikz.Generator.Global
             {
                 // PGFPlots Options
                 foreach (var option in this)
+                {
+                    if (string.IsNullOrEmpty(option)) // Skip empty options
+                        continue;
+
                     yield return $"\\pgfplotsset{{{option}}}";
+                }
             }
         }
 
---
+++ ILN2Tikz/Generator/TikhzFormatUtility.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using ILN2Tikz.Generator.Global;
+using ILNumerics.Drawing;
+
+namespace ILN2Tikz.Generator
+{
+    public static class TikzFormatUtility
+    {
+        #region Line
+
+        internal static string FormatLine(Color color, DashStyle style, float width)
+        {
+            return FormattableString.Invariant($"color={Globals.Colors.GetColorName(color)},{FormatDashStyle(style)},line width={width:F1}pt");
+        }
+
+        internal static string FormatLine(Color color, DashStyle style, int width)
+        {
+            return FormattableString.Invariant($"color={Globals.Colors.GetColorName(color)},{FormatDashStyle(style)},line width={width}pt");
+        }
+
+        internal static string FormatDashStyle(DashStyle style)
+        {
+            switch (style)
+            {
+                case DashStyle.Solid:
+                    return "solid";
+                case DashStyle.Dashed:
+                    return "dashed";
+                case DashStyle.PointDash:
+                    return "densely dashed";
+                case DashStyle.Dotted:
+                    return "dotted";
+                default:
+                    return "densely dotted"; // Fallback style
+            }
+        }
+
+        #endregion
+
+        #region Marker
+
+        internal static string FormatMarker(Color color, MarkerStyle style, int size)
+        {
+            if (style == MarkerStyle.None)
+                return "";
+
+            return $"mark={FormatMarkerStyle(style)},mark size={size},mark options={{fill={Globals.Colors.GetColorName(color)}}}";
+        }
+
+        internal static string FormatMarkerStyle(MarkerStyle style)
+        {
+            switch (style)
+            {
+                case MarkerStyle.Dot:
+                    return "*";
+                case MarkerStyle.Circle:
+                    return "o";
+                case MarkerStyle.Diamond:
+                    return "diamond*";
+                case MarkerStyle.Square:
+                    return "square*";
+                case MarkerStyle.Rectangle:
+                    return "square*";
+                case MarkerStyle.TriangleUp:
+                    return "triangle*";
+                case MarkerStyle.TriangleDown:
+                    return "triangle*";
+                case MarkerStyle.TriangleLeft:
+                    return "pentagon*";
+                case MarkerStyle.TriangleRight:
+                    return "pentagon*";
+                case MarkerStyle.Plus:
+                    return "oplus*"; //return "+";
+                case MarkerStyle.Cross:
+                    return "otimes*"; //return "x";
+                case MarkerStyle.None:
+                    return "";
+                default:
+                    return "plus";
+            }
+        }
+
+        #endregion
+    }
+}
